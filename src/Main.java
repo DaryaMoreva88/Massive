f343ffaa6fe35069207d8c84610095d3d4cbabdd
moreva <belkastrelka88666@gmail.com>
2022-09-25 15:54:02 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        System.out.println("Задание 1");
+        int [] weight = new int [12];
+        weight [0] = 1;
+        int oneWeight = weight[0];
+        System.out.println(oneWeight);
+        weight [0] = 2;
+        int twoWeight = weight[0];
+        System.out.println(twoWeight);
+        weight [0] = 3;
+        int threeWeight = weight[0];
+        System.out.println(threeWeight);
+        double [] weightTwo = {1.57, 7.654, 9.986};
+        weightTwo [0] = 1.57;
+        double oneWeightTwo = weightTwo[0];
+        System.out.println(oneWeightTwo);
+        weightTwo [0] = 7.654;
+        double twoWeightTwo = weightTwo[0];
+        System.out.println(twoWeightTwo);
+        weightTwo [0] = 9.986;
+        double threeWeightTwo = weightTwo[0];
+        System.out.println(threeWeightTwo);
+        int [] garden = new int [3];
+        garden [0] = 1;
+        int oneGarden = garden [0];
+        System.out.println(oneGarden);
+        garden [0] = 3;
+        int twoGarden = garden [0];
+        System.out.println(twoGarden);
+        garden [0] = 5;
+        int threeGarden = garden [0];
+        System.out.println(threeGarden);
+
+        //Задание 2
+        System.out.println("Задание 2");
+
+    }
+}
